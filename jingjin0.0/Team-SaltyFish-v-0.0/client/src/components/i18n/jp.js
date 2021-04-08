795a37968ecbde0b48da51b9229f6383a8130224
jingjinl <66043269+jingjinl@users.noreply.github.com>
2021-04-08 11:25:23 +0800

    jingjin0.0

---
+++ jingjin0.0/Team-SaltyFish-v-0.0/client/src/components/i18n/jp.js
@@ -0,0 +1,51 @@
+export default{
+  Im:"私は",
+  home:"ホームページ",
+  education:"教育",
+  skills:"腕前",
+  projects:"プロジェクト",
+  subjects:"科目",
+  work:"職歷",
+  contact:"連絡",//contact username more natural than contact me. can use "連絡をお願いします" (please contact me)
+  language:"言語",
+  logout:"ログアウト",
+  reset:"リセット",
+  upload:"アップロード",
+  about_me:"私について",
+  contact_details:"連絡先",　// 連絡先 is a common phrase for contact infomation vs 連絡の詳細 which directly translates to contact's details.
+  add_subjects:"科目に追加",　// directly translates to "add to subjects" rather than "add subject" which makes a little more sense in japanesee now
+  delete:"削除",
+  edit_Intro:"概論を編集", //translates roughly to edit general remarks. I can change the work to edit self introduction if that is more fitting. 
+  work1: "職歴", 
+  add_work:"職歴に追加",
+  welcome:"ウエルカム",
+  login:"ログイン",
+  register:"サインアップ", // more common to you "sign up" in japanese
+  createdby:"作成者", //translates roughly to "creaters" rather than "created by"
+  Createacc:"アカウントを作成する",
+  Resetacc:"アカウントをリセット",
+  registerfa:"もうアカウントをもっていますか？",
+  submit:"送信",
+  edit_Bio:"伝記を編集",
+  edit_phone:"電話番号を編集",
+  upload_transcript:"トランスクリプトをアップロード",
+  download:"成績書をダウンロードするにはクリックしてください",
+  add_edu:"教育を追加",
+  add_projects:"プロジェクトを追加",
+  add_skills:"腕前を追加",
+  gallery:"ギャラリー",
+  add_photodesc:"写真の説明を追加",
+  add_photo:"写真をアップロード",
+
+  intro:"イントロダクション",
+
+  landing1:"ユーザーをお探しですか？ここで検索",
+    landing2:"夢の仕事に近づくために、自分自身を磨こう",
+    landing3:"簡単に仕事を探す",
+    landing4:"Swat Kats は、あなたの学業成績やその他のスキルを通して、あなたのプロとしてのイメージを世界にアピールすることができます。雇用者もこのサイトを見ています。",
+    landing5:"あなたのイメージをパーソナル化する",
+    landing6:"学校での成績だけではなく、あなたの全人的な成果をアピールしてください。あなたのギャラリーに画像をアップロードして、あなたの自由な時間に何をしているのかを私たちに見せてください。",
+    landing7:"異なる言語のサポート",
+    landing8:"私たちは皆、異なる文化的背景から来ており、異なる言語を話すことを理解しています。これに対応するために、北京語と日本語の翻訳をサポートしています。"
+
+}
---
+++ jingjin0.0/Team-SaltyFish-v-0.0/routes/router.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('../controller.js');
+
+//Routes for all backend acccess
+
+router.post('/register', controller.createUser);
+router.post('/reset', controller.resetUser);
+router.post('/login', controller.loginUser);
+//profile1 is to send user data on login
+//profile2 is to look up users in search
+router.get('/profile1/:user',controller.getProfile);
+router.get('/profile2/:email', controller.findUserByEmail);
+//routes for editing user deets
+router.put('/profilebio/:user',controller.addBio);
+router.put('/profilework/:user',controller.addWork);
+router.put('/profilephone/:user',controller.addPhone);
+router.put('/profileintro/:user',controller.addIntro);
+router.put('/profileproject/:user',controller.addproject);
+router.put('/profileskills/:user',controller.addSkills);
+router.put('/profileedu/:user',controller.addEducation);
+router.put('/profilesub/:user',controller.addSubjects);
+router.put('/addprofilepic/:user',controller.addProfilePicture);
+router.put('/addtranscript/:user',controller.addTranscript);
+
+router.put('/addtogallery/:user',controller.addGallery);
+router.put('/deletefromgallery/:user', controller.findGalleryPicAndDelete);
+
+router.put('/addweb/:user',controller.addweb);
+router.put('/namechange/:user',controller.changeName);
+
+router.put('/deleteedu/:user',controller.deleteEducation);
+router.put('/findanddeletsub/:user',controller.findSubjectsAndDelete);
+router.put('/findanddeletwork/:user',controller.findWorkAndDelete);
+router.put('/findanddeletproject/:user',controller.findProjectAndDelete);
+router.put('/findanddeletskill/:user',controller.findSkillAndDelete);
+
+
+
+
+// Find all users
+router.get('/users', controller.findAllUsers);
+ 
+// Find one user by id
+router.get('/users/id/:id', controller.findOneUser);
+ 
+//Find one user by name
+router.get('/users/name/:name', controller.FuzzySearchUserName);
+module.exports = router;
