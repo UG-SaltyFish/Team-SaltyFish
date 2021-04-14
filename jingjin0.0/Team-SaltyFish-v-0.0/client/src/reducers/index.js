6fb6fc833340a978a3ce42b580fd88c08f404299
Joyce888888 <72294513+Joyce888888@users.noreply.github.com>
2021-04-15 00:36:24 +0800

    delete some useless files

--- jingjin0.0/Team-SaltyFish-v-0.0/client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./authReducer";
-
-
-export default combineReducers({
-    // This what the state object is whenever you call mapStateToProps. When you connect these
-    // get linked to that component
-    auth: authReducer
-});
