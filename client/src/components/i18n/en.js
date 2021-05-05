9508f866708b7543aebdf732e7b4dca04915bf12
jingjinl <66043269+jingjinl@users.noreply.github.com>
2021-05-05 21:27:02 +0800

    q

--- client/src/components/i18n/en.js
+++ client/src/components/i18n/en.js
@@ -50,7 +50,7 @@ export default{
     landing3:"Find jobs easily",
     landing4:"Swat Kats&Salty Fish allows you to present your professional image to the world, through your academic achievements and other skills. Employers watch this site too, you never know what opportunities lie around the corner!",
     landing5:"Personalise your image",
-    landing6:"On Swat Kats&Sailty Fish, we don't just care about your results in school, we want you to show off your holistic accomplishments as well. Upload images to your gallery and show us what you get up to in your free time!",
+    landing6:"On Swat Kats&Salty Fish, we don't just care about your results in school, we want you to show off your holistic accomplishments as well. Upload images to your gallery and show us what you get up to in your free time!",
     landing7:"Support for different languages",
     landing8:"We understand that we all come from different cultural backgrounds and speak different languages. To accommodate this, we have translations supported for Mandarin and Japanese - click the 'Language Options' button above!"
 
